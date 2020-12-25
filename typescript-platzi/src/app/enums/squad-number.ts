c22630fe06d31ce74b211755a367b16009807487
LDHugeman <luisdavid.vp@gmail.com>
2020-12-25 16:03:10 +0100

    clases 20

---
+++ typescript-platzi/src/app/enums/squad-number.ts
@@ -0,0 +1,13 @@
+export enum SquadNumber {
+  goalKeeper = 1,
+  rightBack = 4,
+  sweeper = 2,
+  stopper = 6,
+  leftBack = 3,
+  rightMidFielder = 8,
+  centarlDefensiveMidFielder = 5,
+  leftMidFielder = 10,
+  rightWinger = 7,
+  centreForward = 9,
+  leftWinger = 11,
+}
